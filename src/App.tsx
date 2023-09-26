4fcc8b69f9aa377ae185b1ff0c878c86e8f979f9
Yannis Baltus <yannis.baltus@outlook.com>
2023-09-26 12:49:09 +0200

    🚧 Save App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React, {FunctionComponent, useState} from 'react';
+import React, {FunctionComponent, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Character from './models/character';
 import CHARACTERS from './models/mock-character';
 
 const App: FunctionComponent = () => {
-  const [listCharacters] = useState<Character[]>(CHARACTERS);
+  const [listCharacters, setListCharacters] = useState<Character[]>([]);
+
+  // Initialize listCharacters
+  useEffect(() => {
+    setListCharacters(CHARACTERS);
+  }, [])
 
   return (
     <div>
       <h1>One Piece App</h1>
-          <p>Bonjour, Il y a {listCharacters.length} personnages !</p>
-
-      </div>
+      <p>Bonjour, Il y a {listCharacters?.length} personnages !</p>
+      <ul>
+        {listCharacters.map(({id, name}) => {
+          return <li key={id+'-'+name}>{name}</li>
+        })}
+      </ul>
+    </div>
   );
 }
 
